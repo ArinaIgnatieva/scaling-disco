a373aa8a45b804977bc69cc45a265cada308aa0b
ArinaIgnatieva <114217585+ArinaIgnatieva@users.noreply.github.com>
2022-12-12 19:14:57 +0300

    Update laba.java

--- laba.java
+++ laba.java
@@ -3,43 +3,39 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Scanner Scan = new Scanner(System.in);
-            
-            System.out.print("Введите размерность массива:");
-            int n = Scan.nextInt();
 
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                System.out.print("Введите элемент массива:");
-                arr[i] = Scan.nextInt();
-            }
+        Scanner Scan = new Scanner(System.in);
 
-            System.out.print("Введите число, которое нужно удалить:");
-            int del = Scan.nextInt();
+        System.out.print("Введите размерность массива:");
+        int n = Scan.nextInt();
 
-
-            int arr2_size = 0;
-
-            for (int i = 0; i < arr.length; i++) {
-                if (del != arr[i]) {
-                    arr2_size++;
-                }
-            }
-            int[] arr2 = new int[arr2_size];
-            int j = 0;
-
-            for (int i = 0; i < arr.length; i++) {
-                if (del != arr[i]) {
-                    arr2[j++] = arr[i];
-                }
-            }
-            
-            System.out.println("Исходный массив:" + Arrays.toString(arr));
-            System.out.println("Массив после обработки: " + Arrays.toString(arr2));
+        int [] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Введите элемент массива:");
+            arr[i] = Scan.nextInt();
         }
-        catch (Exception e){
-            System.out.print("Ошибка, числа должны быть целыми");
+
+        System.out.print("Введите число, которое нужно удалить:");
+        int del = Scan.nextInt();
+
+        int arr2_size = 0;
+
+        for (int i = 0; i < arr.length; i++){
+            if (del != arr[i]){
+                arr2_size++;
+            }
         }
+        int [] arr2 = new int[arr2_size];
+        int j = 0;
+
+        for (int i = 0; i < arr.length; i++){
+            if (del!=arr[i]){
+                arr2[j++]=arr[i];
+            }
+        }
+
+        System.out.println("Исходный массив:" + Arrays.toString(arr));
+        System.out.println("Массив после обработки: " + Arrays.toString(arr2));
+
     }
 }
