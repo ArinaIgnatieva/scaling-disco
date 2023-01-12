fe31e1fc80c2b0889250eecfc1b20443fc119788
ArinaIgnatieva <114217585+ArinaIgnatieva@users.noreply.github.com>
2023-01-12 15:19:16 +0300

    Update and rename lr3.2.java to lab3.2.java

---
+++ lab3.2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner Scan = new Scanner(System.in);
+
+            System.out.print("Введите размерность массива:");
+            int n = Scan.nextInt();
+
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                System.out.print("Введите элемент массива:");
+                arr[i] = Scan.nextInt();
+            }
+
+            System.out.print("Введите число, которое нужно удалить:");
+            int del = Scan.nextInt();
+
+
+            int arr2_size = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] != del) {
+                    arr2_size++;
+                }
+            }
+            int[] arr2 = new int[arr2_size];
+            int j = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] != del) {
+                    arr2[j++] = arr[i];
+                }
+            }
+
+            System.out.println("Исходный массив:" + Arrays.toString(arr));
+            System.out.println("Массив после обработки: " + Arrays.toString(arr2));
+        }
+        catch (Exception e){
+            System.out.print("Ошибка: числа должны быть целыми");
+        }
+    }
+}
